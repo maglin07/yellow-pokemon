27507b097a03af5a21ed00aea5acb93dc6b374d5
Dean Nevins <itsdeannevins@gmail.com>
2021-07-11 22:43:22 -0400

    Able to successfully upload avatar image when signing up.

--- authentication/views.py
+++ authentication/views.py
@@ -10,10 +10,16 @@ from django.views.generic import TemplateView
 
 def signup_view(request):
     if request.method == "POST":
-        form = SignUpForm(request.POST,request.FILES)
+        form = SignUpForm(request.POST, request.FILES)
         if form.is_valid():
             data = form.cleaned_data
-            new_user = Author.objects.create_user(username=data.get("username"), password=data.get("password"), first_name=data.get("first_name"), last_name=data.get("last_name"), avatar_image=data.get("ImageField"))
+            new_user = Author.objects.create_user(
+                username=data.get("username"),
+                password=data.get("password"),
+                first_name=data.get("first_name"),
+                last_name=data.get("last_name"),
+                avatar_image=data.get("avatar_image"),
+            )
             login(request, new_user)
             return HttpResponseRedirect(reverse("homepage"))  
         else:
@@ -36,8 +42,8 @@ class LoginView(TemplateView):
             if user:
                 login(request, user)
                 return HttpResponseRedirect(request.GET.get('next', reverse('homepage')))
-            else:
-                return render(request, "generic_form.html", {"form": form})
+            
+            return render(request, "generic_form.html", {"form": form})
 
 def logout_view(request):
     logout(request)
